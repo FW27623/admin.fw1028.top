006a49085b2731d9dcaccbb34d250b258ebe5d6c
FangWei <admin@fw1028.top>
2021-11-28 21:34:48 +0800

    Improve the prompt sentence，make the reminder clear.

--- src/test_grade/Calc.java
+++ src/test_grade/Calc.java
@@ -6,13 +6,13 @@ class Calc {
     public static void main(String[] args) {
         double[] a;
         double[] b;
-        String str1 = JOptionPane.showInputDialog("请输入打分数据，以空格分隔");
+        String str1 = JOptionPane.showInputDialog("请输入打分数据，必须以空格分隔!\n结果保留两位小数！");
         String[] str_a=str1.split(" ");
         a=new double[str_a.length];
         for(int i=0;i<str_a.length;i++) {
             a[i]=Double.parseDouble(str_a[i]);
         }
-        String str2 = JOptionPane.showInputDialog("请输入体重数据，以空格分隔");
+        String str2 = JOptionPane.showInputDialog("请输入体重数据，必须以空格分隔!\n结果保留两位小数！");
         String[] str_b=str2.split(" ");
         b=new double[str_b.length];
         for(int i=0;i<str_b.length;i++) {
