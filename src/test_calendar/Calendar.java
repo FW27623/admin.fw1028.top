3f62d1a4909699e98c13abb99ec72922f4c19e03
FangWei <admin@fw1028.top>
2021-12-08 23:20:18 +0800

    Update README.md,add more information about the topic.

---
+++ src/test_calendar/Calendar.java
@@ -0,0 +1,64 @@
+package test_calendar;
+
+import javax.swing.*;
+
+/**
+ * @author FangWei
+ */
+public class Calendar {
+    public static void main(String[] args) {
+        int year = -1;
+        int month = -1;
+        while (year < 0) {
+            try {
+                year = Integer.parseInt(JOptionPane.showInputDialog("请输入年份！"));
+            } catch (Exception ex) {
+                year = Integer.parseInt(JOptionPane.showInputDialog("输入错误,请重新输入年份！"));
+            }
+        }
+        while (month < 0) {
+            try {
+                month = Integer.parseInt(JOptionPane.showInputDialog("请输入月份！"));
+                if (month < 1 || month > 12) {
+                    throw new Exception();
+                }
+            } catch (Exception ex) {
+                month = Integer.parseInt(JOptionPane.showInputDialog("输入错误，请重新输入月份！"));
+                System.out.println(",请输入月份");
+            }
+        }
+        System.out.println("你输入的年份和月份是："+year+"年"+month+"月\n对应的日历页如下：");
+        System.out.println("一\t二\t三\t四\t五\t六\t日\n");
+        java.util.Calendar calendar = java.util.Calendar.getInstance();
+        calendar.set(year, month-1, 0);
+        int week = calendar.get(java.util.Calendar.DAY_OF_WEEK);
+        int monthDays;
+        switch (month) {
+            case 1:
+                if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
+                    monthDays = 29;
+                } else {
+                    monthDays = 28;
+                }
+                break;
+            case 2:
+                monthDays = 30;
+                break;
+            default:
+                monthDays = 31;
+        }
+        int weekLoop = 0;
+        for (int i = -week + 2; i <= monthDays; i++) {
+            if (i <= 0) {
+                System.out.print("");
+            } else {
+                System.out.print(i);
+            }
+            weekLoop++;
+            System.out.print("\t");
+            if (weekLoop % 7 == 0) {
+                System.out.println();
+            }
+        }
+    }
+}
---
+++ src/test_calendar/CalendarTest.java
@@ -0,0 +1,50 @@
+package test_calendar;
+
+import static java.util.Calendar.*;
+
+/**
+ * @author FangWei
+ */
+public class CalendarTest {
+    public static void main(String[] args) {
+        System.out.println(getCalendar(2017, 7));
+    }
+
+    public static String getCalendar(int year,int month){
+        StringBuilder c = new StringBuilder();
+        c.append("一\t二\t三\t四\t五\t六\t日\n");
+        month -=1;
+        java.util.Calendar d = getInstance();
+        d.set(year, month, 0);
+
+        int days = 0;
+        switch (month) {
+            case JANUARY,MARCH,MAY,JULY,AUGUST,OCTOBER,DECEMBER:
+                days = 31;
+                break;
+            case APRIL,JUNE,SEPTEMBER,NOVEMBER:
+                days = 30;
+                break;
+            case FEBRUARY:
+                days = 28;
+                if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+                    days = 29;
+                }
+                break;
+            default:
+                System.out.println("输入错误，请重新输入！");
+                System.exit(1);
+        }
+        int week = d.get(DAY_OF_WEEK);
+        System.out.println(week);
+
+        c.append("\t".repeat(Math.max(0, week - 1)));
+        for (int i = 1; i <= days; i++) {
+            c.append(i).append("\t");
+            if((week+i-1)%7 == 0){
+                c.append("\n");
+            }
+        }
+        return c.toString();
+    }
+}
